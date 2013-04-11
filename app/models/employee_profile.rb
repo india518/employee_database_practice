d90f269bb0c0f4decf4c1704271fb03cb80dfd49
India Meisner <india.rae@gmail.com>
2013-04-11 14:42:22 -0700

    About to start making nested form in employee/new for employee profile

--- app/models/employee_profile.rb
+++ app/models/employee_profile.rb
@@ -1,6 +1,6 @@
 class EmployeeProfile < ActiveRecord::Base
-  attr_accessible :employee_id, :height, :age, :favorite_food, :salary, :photo,
-                  :favorite_day_of_week, :birth_date
+  attr_accessible :employee_id, :height_feet, :height_inches, :age, :birth_date,
+                  :favorite_food, :salary, :photo, :favorite_day_of_week,
 
   belongs_to :employee
 end
