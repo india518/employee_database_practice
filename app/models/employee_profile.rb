54f778dfdaadaccbbe1ea331f34124c5eb18d461
India Meisner <india.rae@gmail.com>
2013-04-11 11:34:05 -0700

    Add controllers for Employees, Teams, EmployeeProfiles.

--- app/models/employee_profile.rb
+++ app/models/employee_profile.rb
@@ -1,4 +1,4 @@
 class EmployeeProfile < ActiveRecord::Base
   attr_accessible :employee_id, :height, :age, :favorite_food, :salary, :photo,
-                  :favorite_day_of_week
+                  :favorite_day_of_week, :birth_date
 end
