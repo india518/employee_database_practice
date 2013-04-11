6c2ee2b048754c368e8da004c93d700f6e8624c3
India Meisner <india.rae@gmail.com>
2013-04-11 11:18:49 -0700

    Add stuff to EmployeeProfile

---
+++ db/migrate/20130411180458_add_stuff_to_employee_profile.rb
@@ -0,0 +1,19 @@
+class AddStuffToEmployeeProfile < ActiveRecord::Migration
+  def up
+    add_column :employee_profiles, :height, :string
+    add_column :employee_profiles, :age, :integer
+    add_column :employee_profiles, :favorite_food, :string
+    add_column :employee_profiles, :favorite_day_of_week, :string
+    add_column :employee_profiles, :salary, :decimal
+    add_column :employee_profiles, :photo, :binary
+  end
+  
+  def down
+    remove_column :employee_profiles, :height
+    remove_column :employee_profiles, :age
+    remove_column :employee_profiles, :favorite_food
+    remove_column :employee_profiles, :favorite_day_of_week
+    remove_column :employee_profiles, :salary
+    remove_column :employee_profiles, :photo
+  end
+end
