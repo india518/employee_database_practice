6da32ec278e2559392ce440160f7a6aeef6a80b3
Dylan Patrick Clark <dylanclark112@gmail.com>
2013-04-11 23:33:40 -0700

    Update team_membership.rb

--- app/models/team_membership.rb
+++ app/models/team_membership.rb
@@ -1,7 +1,7 @@
 class TeamMembership < ActiveRecord::Base
   attr_accessible :employee_id, :team_id
   
-  validates :team_id, :uniqueness => { :scope => :employee_id }
+  validates :team_id, :uniqueness => { :scope => :employee_id } #REV: cool
   
   belongs_to :employee
   belongs_to :team
